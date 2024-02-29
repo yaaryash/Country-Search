f48b15826a87dba015b67085ee871f71bc69a5f5
yaaryash <yashwalke3434@gmail.com>
2024-02-29 17:30:59 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [countries, setCountries] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://restcountries.com/v3.1/all");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setCountries(data);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  const filteredCountries = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(search.toLowerCase())
+  );
+  const countryCard = {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+  };
+
+  const cardStyle = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: "10px",
+    padding: "10px",
+    border: "1px solid #ccc",
+    borderRadius: "10px",
+    width: "200px",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Search for countries..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+      <div style={countryCard}>
+        {filteredCountries.length > 0 ? (
+          filteredCountries.map((item) => (
+            <div key={item.cca3} style={cardStyle} className="countryCard">
+              <img
+                src={item.flags.png}
+                alt={`Flag of ${item.name.common}`}
+                style={{ width: "100px", height: "100px" }}
+                width="100"
+                height="100"
+              />
+              <h2>{item.name.common}</h2>
+            </div>
+          ))
+        ) : (
+          <div>No countries found</div>
+        )}
+      </div>
+    </>
   );
 }
-
-export default App;
